7adb36df26000e685563e4554ec735ecccde506d
Yohannes Libanos <yohannes@fluxsoft.dev>
2020-08-14 23:27:40 +0100

    Add middlewares

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -4,12 +4,22 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
 	"github.com/yohannesHL/go-marketplace/pkg/server/products"
 )
 
 // Start starts the server.
 func Start(host string) {
 	router := chi.NewRouter()
+
+	router.Use(
+		render.SetContentType(render.ContentTypeJSON), 
+		middleware.AllowContentEncoding("application/json"),
+		middleware.RequestID,
+		middleware.RealIP,
+		middleware.Recoverer,
+	)
 
 	router.Route("/products", func(r chi.Router) {
 		products.RegisterRoutes(r)
